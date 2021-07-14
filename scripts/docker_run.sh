2ed69f7c254657ab76546c3d0abe9bdb93dfe20e
oneshut <>
2021-07-14 11:30:13 +0800

    add nulink-core node

---
+++ scripts/docker_run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This script is meant to be run on Unix/Linux based systems
+set -e
+
+echo "*** Start Nulink Node ***"
+
+cd $(dirname ${BASH_SOURCE[0]})/..
+
+docker-compose down --remove-orphans
+docker-compose run --rm --service-ports dev $@
