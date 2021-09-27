d4fb8a640e430a7fbd24cf031f6bbccd8a1c214e
oneshut <>
2021-09-27 11:50:34 +0800

    remove unused code

--- README.md
+++ README.md
@@ -155,7 +155,7 @@ the following:
 
 The runtime in this project is constructed using many FRAME pallets that ship with the
 [core Substrate repository](https://github.com/paritytech/substrate/tree/master/frame) and a
-template pallet that is [defined in the `pallets`](./pallets/template/src/lib.rs) directory.
+template pallet that is [defined in the `pallets`](pallets/utils/src/lib.rs) directory.
 
 A FRAME pallet is compromised of a number of blockchain primitives:
 
--- pallets/policy/src/lib.rs
+++ pallets/policy/src/lib.rs
@@ -55,13 +55,6 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	// The pallet's runtime storage items.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
-
 	#[pallet::storage]
 	#[pallet::getter(fn policys)]
 	/// Metadata of an staker.
@@ -104,45 +97,8 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T:Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
-
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn crate_policy(origin: OriginFor<T>,pid: PolicyID,
+		pub fn create_policy(origin: OriginFor<T>,pid: PolicyID,
 		period: T::BlockNumber,stakers: Vec<T::AccountId>) -> DispatchResult {
 			let owner = ensure_signed(origin)?;
 			Self::base_create_policy(owner,pid,period,stakers)
@@ -167,13 +123,13 @@ impl<T: Config> Pallet<T>  {
 		// reserve the asset
 
 		Polices::<T>::insert(pid, PolicyInfo{
-			pID:	pid,
+			pID:	pid.clone(),
 			policyPeriod: period + frame_system::Pallet::<T>::block_number(),
 			policyStop: period + frame_system::Pallet::<T>::block_number(),
 			policyOwner: owner,
 			stackers: stakers.clone(),
 		});
-		T::PolicyHandle::create_policy(owner.clone(),amount,pid)?;
+		T::PolicyHandle::create_policy(owner.clone(),amount,pid.clone())?;
 		// Emit an event.
 		Self::deposit_event(Event::CreateNewPolicy(pid, owner.clone()));
 
