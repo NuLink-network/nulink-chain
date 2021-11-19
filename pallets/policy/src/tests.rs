ed425cc8e29801ba30ea2afad1019f24f1e81d81
oneshut <>
2021-11-19 17:04:13 +0800

    add it_works_for_revoke_policy

--- pallets/policy/src/tests.rs
+++ pallets/policy/src/tests.rs
@@ -53,3 +53,29 @@ fn it_works_for_create_policy2() {
 		assert_noop!(NulinkPolicy::base_create_policy(1,100,100,20,stakers.clone()),Error::<Test>::RepeatPolicyID);
 	});
 }
+
+#[test]
+fn it_works_for_revoke_policy() {
+	new_test_ext().execute_with(|| {
+		// create policy and revoke it
+		let stakers = vec![2,3,4,5];
+		let pid = 100;
+		assert_ok!(NulinkPolicy::base_create_policy(1,pid,100,20,stakers.clone()));
+		let p_info0 = NulinkPolicy::get_policy_info_by_pid(100).unwrap();
+		assert_eq!(p_info0.pID,100);
+		assert_eq!(p_info0.policyOwner,1);
+		assert_eq!(p_info0.period,20);
+		assert_eq!(p_info0.stackers,stakers);
+		// the policyStop was 21 block number
+		assert_eq!(p_info0.policyStop,21);
+		// the was no policy id
+		assert_noop!(NulinkPolicy::base_revoke_policy(99,1),Error::<Test>::NotFoundPolicyID);
+		// the revoke block number must large the policy.policyStart
+		assert_noop!(NulinkPolicy::base_revoke_policy(pid,1),Error::<Test>::PolicyOverPeriod);
+		frame_system::Pallet::<Test>::set_block_number(10);
+		assert_ok!(NulinkPolicy::base_revoke_policy(pid,1));
+
+		let p_info1 = NulinkPolicy::get_policy_info_by_pid(100).unwrap();
+		assert_eq!(p_info1.policyStop,10);
+	});
+}
