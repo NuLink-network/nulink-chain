f4a1a8f30c77367ed0e450ad91af33623d59fbc9
oneshut <>
2021-11-19 14:43:49 +0800

    add it_works_for_create_policy

--- pallets/policy/src/tests.rs
+++ pallets/policy/src/tests.rs
@@ -21,3 +21,17 @@ fn correct_error_for_none_value() {
 		// );
 	});
 }
+#[test]
+fn it_works_for_create_policy() {
+	new_test_ext().execute_with(|| {
+		// create policy
+		let stakers = vec![2,3,4,5];
+		assert_ok!(NulinkPolicy::base_create_policy(1,100,100,20,stakers));
+		assert_noop!(NulinkPolicy::get_policy_info_by_pid(999),Error::<Test>::NotFoundPolicyID);
+		let p_info0 = NulinkPolicy::get_policy_info_by_pid(100).unwrap();
+		assert_eq!(p_info0.pID,100);
+		assert_ne!(p_info0.pID,199);
+		assert_eq!(p_info0.policyOwner,1);
+		assert_eq!(p_info0.period,20);
+	});
+}
