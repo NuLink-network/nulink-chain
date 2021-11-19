7dc93aba756177ba090eb698c02c2c1feb8b22be
oneshut <>
2021-11-19 15:30:58 +0800

    update it_works_for_create_policy2

--- pallets/policy/src/tests.rs
+++ pallets/policy/src/tests.rs
@@ -21,17 +21,35 @@ fn correct_error_for_none_value() {
 		// );
 	});
 }
+fn it_works_for_create_policy1() {
+	new_test_ext().execute_with(|| {
+		// create policy
+		let mut stakers = vec![0];
+		for i in 1..100 as u64 {
+			let pid = (i + 1000) as u128;
+			assert_ok!(NulinkPolicy::base_create_policy(i,pid,300,50,stakers.clone()));
+			let p_info0 = NulinkPolicy::get_policy_info_by_pid(pid).unwrap();
+			assert_eq!(p_info0.pID,pid);
+			assert_eq!(p_info0.policyOwner,i);
+			assert_eq!(p_info0.stackers,stakers.clone());
+			stakers.push(i);
+		}
+	});
+}
+
 #[test]
-fn it_works_for_create_policy() {
+fn it_works_for_create_policy2() {
 	new_test_ext().execute_with(|| {
 		// create policy
 		let stakers = vec![2,3,4,5];
-		assert_ok!(NulinkPolicy::base_create_policy(1,100,100,20,stakers));
+		assert_ok!(NulinkPolicy::base_create_policy(1,100,100,20,stakers.clone()));
 		assert_noop!(NulinkPolicy::get_policy_info_by_pid(999),Error::<Test>::NotFoundPolicyID);
 		let p_info0 = NulinkPolicy::get_policy_info_by_pid(100).unwrap();
 		assert_eq!(p_info0.pID,100);
 		assert_ne!(p_info0.pID,199);
 		assert_eq!(p_info0.policyOwner,1);
 		assert_eq!(p_info0.period,20);
+		assert_eq!(p_info0.stackers,stakers);
+		assert_noop!(NulinkPolicy::base_create_policy(1,100,100,20,stakers.clone()),Error::<Test>::RepeatPolicyID);
 	});
 }
